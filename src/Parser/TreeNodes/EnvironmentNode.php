7cdddf313a632af7192dff15e9c9c386ccca5eaf
tkleinbauer <thomas.kleinbauer@dagstuhl.de>
2026-01-19 12:19:58 +0100

    Removed unused variable.

--- src/Parser/TreeNodes/EnvironmentNode.php
+++ src/Parser/TreeNodes/EnvironmentNode.php
@@ -4,6 +4,8 @@ namespace Dagstuhl\Latex\Parser\TreeNodes;
 
 class EnvironmentNode extends EnvelopeNode
 {
+    private string $name;
+
     public function __construct(int $lineNumber, public string $envName, CommandNode $opening, CommandNode $closing)
     {
         parent::__construct($lineNumber, $opening, $closing);
