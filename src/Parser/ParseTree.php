85ac26d51f777a4d9bd101ff0e3522b372e447ec
didas <didas@dagstuhl.de>
2026-02-18 14:59:18 +0100

    ParseTree: remove surrounding curly braces from arguments in collectMacros

--- src/Parser/ParseTree.php
+++ src/Parser/ParseTree.php
@@ -135,7 +135,7 @@ class ParseTree
                             }
                         }
                     } else {
-                        $arguments[] = $child->toLatex();
+                        $arguments[] = preg_replace('/^\{(.*)}$/', '$1', $child->toLatex());
                     }
                 }
             }
